898aec91b250cf8bf13f41daeedf2897ee3b1eaf
tasmto <mtokotashinga@gmail.com>
2023-07-09 21:04:29 +0200

    manually set dist

--- apps/ba-website/next.config.js
+++ apps/ba-website/next.config.js
@@ -1,21 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: ['ui'],
+  transpilePackages: ["ui"],
   reactStrictMode: true,
   swcMinify: true,
+  distDir: "../.next",
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'cdn.sanity.io',
+        protocol: "https",
+        hostname: "cdn.sanity.io",
       },
       {
-        protocol: 'https',
-        hostname: 'www.cdn.sanity.io',
+        protocol: "https",
+        hostname: "www.cdn.sanity.io",
       },
       {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
+        protocol: "https",
+        hostname: "images.unsplash.com",
       },
     ],
   },
